5d4264da47a78c24b72c38466b9f545b12c2c82f
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-10-30 14:13:08 -0600

    trying to finish problem 12

--- index.js
+++ index.js
@@ -269,3 +269,44 @@ function guessGifts (wishlist, presents) {
 
 
 // console.log(guessGifts(wishlist, presents)); // must return ["Toy Car", "Mini Puzzle"]
+
+
+//Problem 12
+
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from ato z.
+
+
+//DID NOT FINISH TRY AGAIN 
+
+function printerError(s) {
+  let compare = ['a','b','c','d','e','f','g','h','i','j','k','l','m']
+  let errors = 0
+  let length = s.length
+    for (let i = 0; i < s.length; i++) {
+        for (let k = 0; k < s.length; k++) {
+            if (s[i] === compare[k]) {
+              continue
+            } else {
+              errors++
+            }
+        }
+       let errors2 = String(errors)
+       let length2 = String(length)
+       let result = errors2 + '/' + length
+       console.log(result)
+      
+      return result
+}
+
+}
+
+
+// s="aaabbbbhaijjjm"
+// printer_error(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// printer_error(s) => "8/22"
