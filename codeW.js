68d7d6f6a65a34121c5947eb60fb352a7f099fad
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-11-09 10:34:38 -0700

    Finished coding challange week7 day3

--- codeW.js
+++ codeW.js
@@ -12,4 +12,32 @@ function disemvowel (str) {
     return answer
 }
 
-  console.log(disemvowel('This website is for losers Lol!'))
+//   console.log(disemvowel('This website is for losers Lol!'))
+
+//   problem 2
+
+// This is a short-length challenge, but requires some clever thinking.
+
+// Given two lists, find the smallest difference (subtraction) between any two nums.
+
+// For example, given the arrays:
+
+// The result would be 3, since 23 - 20 = 3 is the smallest difference of any pair of numbers in those lists.
+
+// Write a function that determines the smallest difference.
+
+function diff (arr1, arr2) {
+    let answer
+    let compare = []
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+             compare.push(arr2[j] - arr1[i])
+        }
+    }
+    // console.log(compare)
+     answer = Math.min(...compare)
+    //  console.log(answer)
+     return answer
+}
+
+console.log(diff([10, 20, 14, 16, 18], [30, 23, 54, 33, 96]))
