633299e005c4091324837d2615a2fe01e645bdeb
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-11-16 10:49:27 -0700

    Finished 2 code wars problmes

--- codeW.js
+++ codeW.js
@@ -161,4 +161,39 @@ function isPalindrome(x) {
       }
       return answer 
     }
+
+
+    // Problem 8 
+//     Messi is a soccer player with goals in three leagues:
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+    function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+      return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
       
+
+  // problem 9
+
+//   Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// For example, if this array were passed as an argument:
+
+// ["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+// Your function would return the following array:
+
+// ["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+// All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
+  function sortByLength (array) {
+      let answer = array.sort( (a,b) => {
+       return a.length - b.length
+      })
+      return answer
+  };
+
+  // console.log(sortByLength(["Telescopes", "Glasses", "Eyes", "Monocles"]))
